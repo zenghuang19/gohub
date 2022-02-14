1ab273928cdbd64da3f63536116198d9dfe18a6d
zeng <zenghuang19@163.com>
2022-02-14 16:18:44 +0800

    配置方案实现

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 )
 
+func init()  {
+	//加载配置
+	btsConfig.Initialize()
+}
+
 func main() {
+	//配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
