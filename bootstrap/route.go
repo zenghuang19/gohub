b6eddb2bf50d5bf53119c6e4af74e73dd200af04
zeng <zenghuang19@163.com>
2022-03-14 16:28:43 +0800

    add:访问日志

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"github.com/gin-gonic/gin"
+	"gohub/app/http/middlewares"
 	"gohub/routes"
 	"net/http"
 	"strings"
@@ -20,12 +21,12 @@ func SetupRoute(route *gin.Engine) {
 
 func registerGlobMiddleWare(route *gin.Engine) {
 	route.Use(
-		gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
 
-func setup404Handler(route *gin.Engine)  {
+func setup404Handler(route *gin.Engine) {
 	// 处理404
 	route.NoRoute(func(c *gin.Context) {
 		//获取标头信息的Accept信息
@@ -33,10 +34,10 @@ func setup404Handler(route *gin.Engine)  {
 		if strings.Contains(acceptString, "text/html") {
 			//是html
 			c.String(http.StatusNotFound, "页面返回404")
-		}else {
+		} else {
 			//默认返回JSON
 			c.JSON(http.StatusNotFound, gin.H{
-				"error_code": 404,
+				"error_code":    404,
 				"error_message": "路由未定义，请确认URL和请求方法是否正确。",
 			})
 		}
